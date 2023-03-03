3362e1f1e8400d8987a6755b3664ed5c7c1d5305
Otto Knowsn't <ottodoesntknow@gmail.com>
2023-03-04 00:49:21 +0100

    replace flutter_sound by simpler record package

--- lib/utilities/sound_recorder.dart
+++ lib/utilities/sound_recorder.dart
@@ -1,17 +1,15 @@
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:record/record.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:apneadiag/utilities/local_notifications.dart';
 import 'package:apneadiag/utilities/server_upload.dart';
 import 'package:apneadiag/utilities/app_data.dart';
 
 class SoundRecorder {
-  static final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
+  static final _recorder = Record();
   static String _lastRecordingPath = '';
 
   static Future<void> init() async {
-    await Permission.microphone.request();
-    await _recorder.openRecorder();
+    await _recorder.hasPermission();
   }
 
   static Future<void> start() async {
@@ -19,8 +17,9 @@ class SoundRecorder {
     _lastRecordingPath =
         '${path.path}/${DateTime.now().millisecondsSinceEpoch}.wav';
     await AppData().setIsRecording(true);
-    await _recorder.startRecorder(
-      toFile: _lastRecordingPath,
+    await _recorder.start(
+      path: _lastRecordingPath,
+      encoder: AudioEncoder.wav,
     );
     LocalNotifications.showNotification(
         title: 'Grabación iniciada',
@@ -28,7 +27,7 @@ class SoundRecorder {
   }
 
   static Future<void> stop() async {
-    await _recorder.stopRecorder();
+    await _recorder.stop();
     await AppData().setIsRecording(false);
     await AppData().setLastRecordingPath(_lastRecordingPath);
     LocalNotifications.showNotification(
